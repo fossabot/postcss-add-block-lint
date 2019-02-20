1d07957969e910acd7ff278f548471ecc4e64e6d
Daniel Tedman <dbtedman@gmail.com>
2019-02-20 20:04:49 +1000

    Scaffold basic plugin functionality.

--- src/main.js
+++ src/main.js
@@ -2,4 +2,16 @@
 //
 // Main
 //
+// See https://github.com/postcss/postcss/blob/master/docs/guidelines/plugin.md
+//
 //------------------------------------------------------------------------------
+
+const PostCSS = require("postcss");
+
+module.exports = PostCSS.plugin("postcss-add-block-lint", () => {
+  return () => {
+    return new Promise(resolve => {
+      resolve();
+    });
+  };
+});
--- unit/mainTest.js
+++ unit/mainTest.js
@@ -3,3 +3,14 @@
 // Main Test
 //
 //------------------------------------------------------------------------------
+
+const PostCSS = require("postcss");
+const PostCSSAddBlockLint = require("../src/main");
+
+describe("PostCSS Add Block Lint", () => {
+  it("can be loaded as a plugin", () => {
+    PostCSS([
+      PostCSSAddBlockLint()
+    ]);
+  });
+});
