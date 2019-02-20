b5069f882670b65759d8af5f08bc21b23c40d112
Daniel Tedman <dbtedman@gmail.com>
2019-02-20 20:31:03 +1000

    Add plugin and access selector to prepare for testing.

--- src/main.js
+++ src/main.js
@@ -2,16 +2,22 @@
 //
 // Main
 //
-// See https://github.com/postcss/postcss/blob/master/docs/guidelines/plugin.md
-//
 //------------------------------------------------------------------------------
 
 const PostCSS = require("postcss");
 
-module.exports = PostCSS.plugin("postcss-add-block-lint", () => {
-  return () => {
-    return new Promise(resolve => {
-      resolve();
+module.exports = PostCSS.plugin("postcss-add-block-lint", options => {
+
+  // Overwrite defaults with user defined options.
+  options = Object.assign({}, options);
+
+  // Root (postcss/lib/root.js)
+  return css => {
+    // Rule (postcss/lib/rule.js)
+    css.walkRules(cssRule => {
+      if (options) {
+        cssRule.selector = `${cssRule.selector}`;
+      }
     });
   };
 });
--- unit/mainTest.js
+++ unit/mainTest.js
@@ -4,13 +4,20 @@
 //
 //------------------------------------------------------------------------------
 
+const Assert = require("assert");
 const PostCSS = require("postcss");
 const PostCSSAddBlockLint = require("../src/main");
 
+const okCSS = `
+  .demo {
+    color: red;
+  }
+`;
+
 describe("PostCSS Add Block Lint", () => {
   it("can be loaded as a plugin", () => {
-    PostCSS([
-      PostCSSAddBlockLint()
-    ]);
+    const postCSS = PostCSS([PostCSSAddBlockLint()]);
+
+    Assert.ok(postCSS.process(okCSS).css === okCSS);
   });
 });
